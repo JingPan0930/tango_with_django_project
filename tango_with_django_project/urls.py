307562e2a8c0793ba5e9a0ab5cb3c3e49c95e89f
Jing Pan <2345475p@student.gla.ac.uk>
2019-02-11 04:22:34 +0800

    forth commit

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 """tango_with_django_project URL Configuration
+
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -21,4 +24,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
