f355e2f9d17681a8aaad4317d7f3e792314bc784
Jing Pan <2345475p@student.gla.ac.uk>
2019-02-11 06:35:36 +0800

    sixth commit

--- populate_rango.py
+++ populate_rango.py
@@ -49,8 +49,8 @@ def add_page(cat, title, url, views=0):
        p.views=views
        p.save()
        return p
-def add_cat(name):
-       c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+       c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
        c.save()
        return c
 if __name__ == '__main__':
--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
-                    name = models.CharField(max_length=128, unique=True)
-                  
-                    def __str__(self): 
-                        return self.name 	
+             name = models.CharField(max_length=128, unique=True)
+             views = models.IntegerField(default=0) 
+             likes = models.IntegerField(default=0)
+
+             def __str__(self): 
+                              return self.name 	
 
 class Page(models.Model):
        category = models.ForeignKey(Category)
