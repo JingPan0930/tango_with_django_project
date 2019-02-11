f2ee7ff1d0e4c45a476566b4d041ac803baff8ca
Jing Pan <2345475p@student.gla.ac.uk>
2019-02-11 23:50:11 +0800

    before 10

--- rango/urls.py
+++ rango/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^add_page/$', views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
-        views.show_category, name='show_category'),
+         views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+         views.add_page,
+         name='add_page'),
+    url(r'^register/$',
+         views.register,
+         name='register'),
 ]
